9e7cf0d7aa4e429d0da94d3696af1a9fd64eef01
Federico Del Gaudio <federico.delgaudio@gmail.com>
2021-04-17 23:37:02 +0200

    feat: adds time divisions

---
+++ tidal/Times.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Sound.Tidal.Context
+
+time = 84 :: Pattern Double
+
+n64 = (60 / time) / 16 :: Pattern Double
+
+n32 = (60 / time) / 8 :: Pattern Double
+
+n16 = (60 / time) / 4 + 0.02 :: Pattern Double
+
+n8 = (60 / time) / 2 :: Pattern Double
+
+n4 = (60 / time) :: Pattern Double
+
+n2 = (60 / time) * 2 :: Pattern Double
+
+n1 = (60 / time) * 4 :: Pattern Double
