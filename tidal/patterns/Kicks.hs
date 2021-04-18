d23c501e496e8910e33eeb86a11affe143803709
Federico Del Gaudio <federico.delgaudio@gmail.com>
2021-04-18 23:47:04 +0200

    feat: updates kicks

--- tidal/patterns/Kicks.hs
+++ tidal/patterns/Kicks.hs
@@ -2,4 +2,10 @@
 
 import Sound.Tidal.Context
 
-kick01 = n "[[c3 ~ ~ c3][~ ~ c3 ~][c3 ~ c3 ~][~ ~ ~ ~]]" # sustain n16 # s "midi" # midichan 0 # amp 1 :: Pattern ValueMap
+-- x = note -> ex: n "c3"
+-- y = sustain value -> ex: n16
+-- z = velocity value -> ex: 127
+
+kick01(x, y, z) = struct "[t t t t]" $ x # sustain y # s "midi" # midichan 0 # amp z :: Pattern ValueMap
+
+kick02(x, y, z) = struct "[[t ~ ~ t][~ ~ t ~][t ~ t ~][~ ~ ~ ~]]" $ x # sustain y # s "midi" # midichan 0 # amp z :: Pattern ValueMap
