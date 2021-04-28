b1d440d3f153de0dd0955d44468dae8fb164b3bd
Federico Del Gaudio <federico.delgaudio@gmail.com>
2021-04-28 23:48:22 +0200

    feat: adds instruments stack aliases

---
+++ tidal/Instruments.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Sound.Tidal.Context
+
+sh101 = stack
+
+juno106 = stack
+
+jx3p = stack
+
+system100 = stack
+
+sh2 = stack
+
+promars = stack
+
+tr808 = stack
+
+tr909 = stack
+
+dsDrums = stack
+
+superberry = stack
+
+pigments = stack
+
+sublab = stack
+
+serum = stack
+
+m1 = stack
+
+d50 = stack
+
+dx7 = stack
+
+tr606 = stack
+
+jupiter8 = stack
+
+pendulate = stack
+
+easel = stack
+
+aalto = stack
+
+arp2600 = stack
+
+cs80 = stack
+
+czV = stack
+
+obXa = stack
+
+jup8V = stack
+
+prophetV = stack
