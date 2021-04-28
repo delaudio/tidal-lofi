6187ea0cec45ceae80df11fb7e2d60b129c1bf8a
Federico Del Gaudio <federico.delgaudio@gmail.com>
2021-04-28 23:47:42 +0200

    feat: adds MidiCC ableton list

---
+++ tidal/MidiCC.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Sound.Tidal.Context
+
+-- Ableton Reserved/Suggested CC
+
+-- 1 modulation
+-- 2 breath
+-- 3 (controller)
+-- 4 foot pedal
+-- 5 portamento time
+-- 6 data entry
+-- 7 volume
+-- 8 balance
+-- 9 (controller)
+-- 10 pan
+-- 11 expression
+-- 12 effect control 1
+-- 13 effect control 2
+-- 38 data entry fine
+-- 64 hold pedal
+-- 65 portamento on/off
+-- 66 sostenuto pedal
+-- 67 soft pedal
+-- 68 legato pedal
+-- 69 hold pedal 2
+-- 96 data entry increment
+-- 97 data entry decrement
+-- 98 NRPN LSB
+-- 99 NRPN MSB
+-- 100 RPN LSB
+-- 101 RPN MSB
